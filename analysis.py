00af1adbf7e13eccd97cc522e7526e74a9929442
xzheng902 <xzheng20@colby.edu>
2019-05-03 14:50:13 -0400

    least square fit0

--- analysis.py
+++ analysis.py
@@ -15,9 +15,13 @@ import data
 def obj_func_Hirota(x, baseline, amplitude, damp, phase, period, trend):
     return baseline + amplitude*(np.exp(- damp*x))*np.cos(2*np.pi*(x-phase)/period) + trend*x
 
+# takes in full Data object
+# does not do remove outlier, smooth, detrend etc.
+# NOT CURRENT
 def least_squares_Hirota(full_data):
     # get data from 24-96 hours
     npdata = full_data.windowBaselinedData()
+    # print(npdata.shape)
     # find mid point
     midIdx = int(npdata.shape[0]/2)
 
@@ -26,14 +30,20 @@ def least_squares_Hirota(full_data):
     # estimate trend (slope of the medians of the first half and that of the second half)
     first_half_medians = np.median(npdata[:midIdx,:], axis=0).tolist()[0]
     second_half_medians = np.median(npdata[midIdx:,:], axis=0).tolist()[0]
+    # print(first_half_medians, second_half_medians)
     est_trend = (second_half_medians[1]-first_half_medians[1])/(second_half_medians[0]-first_half_medians[0])
     # estimate baseline
     est_baseline = np.median(npdata, axis=0)[0,1]
+    # print(est_baseline)
     # estimate phase
     # FIX this
     est_phase = npdata[np.argmax(npdata, 0)[0,1],0]
+    # print(est_phase)
     # estimate amplitude
     est_amp = (np.max(npdata,axis=0)[0,1]-np.min(npdata, axis=0)[0,1])/2
+    # print(est_amp)
+    # print(est_trend, est_baseline, est_phase, est_amp)
+
 
     # curve fit
     try:
@@ -41,5 +51,52 @@ def least_squares_Hirota(full_data):
     except:
         return None
 
-
     return popt
+
+
+
+# analysis code 5 methods
+
+# fit0
+# based on original Hirota
+# fit y-(e^(-lambda*t)*A*cos(2*pi*(x-phase)/period)+trend*x+baseline) -> 0
+def obj_func_fit0(params, y, x):
+    baseline = params[0]
+    amplitude = params[1]
+    damp = params[2]
+    phase = params[3]
+    period = params[4]
+    trend = params[5]
+    return ((y - (amplitude*(np.exp(- damp*x))*np.cos(2*np.pi*(x-phase)/period) + trend*x+baseline)).T[0,:])
+
+# fit0
+def fit0(x, y):
+    # print(x.shape)
+    # print(y.shape)
+    midIdx = int(x.shape[0]/2)
+    est_period = 24.0
+    est_damp = 0.005
+    # estimate trend (slope of the medians of the first half and that of the second half)
+    first_half_medians_y = np.median(y[:midIdx,:])
+    second_half_medians_x = np.median(x[midIdx:,:])
+    second_half_medians_y = np.median(y[midIdx:,:])
+    first_half_medians_x = np.median(x[:midIdx,:])
+    # print(first_half_medians_x, first_half_medians_y, second_half_medians_x, second_half_medians_y)
+    est_trend = (second_half_medians_y-first_half_medians_y)/(second_half_medians_x-first_half_medians_x)
+    # print(est_trend)
+    # estimate baseline
+    est_baseline = np.median(y)
+    # print(est_baseline)
+    # # estimate phase
+    # # FIX this
+    est_phase = x[np.argmax(y)][0]
+    # print(est_phase)
+    # estimate amplitude
+    est_amp = (np.max(y)-np.min(y))/2
+    # print(est_amp)
+
+    p0=np.array([est_baseline, est_amp, est_damp, est_phase, est_period, est_trend], dtype=float)
+
+    fit = scipy.optimize.least_squares(obj_func_fit0, p0, args=(y,x))
+
+    return fit.x
--- main.py
+++ main.py
@@ -35,29 +35,35 @@ def main(argv):
         fig = plt.figure()
         ax = fig.add_subplot(111)
         # blue raw data
-        ax.plot(x, y_data, 'b-')
-        params = analysis.least_squares_Hirota(expList[idx])
-        if params is None:
-            print("Curve_fit failed for experiemnt "+str(idx))
+        ax.plot(x, y_data, 'b-', label="data")
+        # params = analysis.least_squares_Hirota(expList[idx])
+        params_fit0 = analysis.fit0(x, y_data)
+        params_fit1 = analysis.fit1(x, y_data)
+        # params_fit0 = analysis.fit0(np.array(expList[idx].windowBaselinedData()[:,0]), np.array(expList[idx].windowBaselinedData()[:,1]))
+        # print("old hirota params: ",params)
+        # print("fit0 params: ", params_fit0)
+        if params_fit0 is None:
+            print("fit0 failed for experiemnt "+str(idx))
         else:
-            y_exp = analysis.obj_func_Hirota(x, params[0], params[1], params[2], params[3], params[4], params[5])
+            y_exp = analysis.obj_func_Hirota(x, params_fit0[0], params_fit0[1], params_fit0[2], params_fit0[3], params_fit0[4], params_fit0[5])
             # red fit
-            ax.plot(x, y_exp, 'r-')
+            ax.plot(x, y_exp, 'r-', label="fit0")
+        plt.legend()
             # green residual
-            ax.plot(x, y_data-y_exp, 'g-')
+            # ax.plot(x, y_data-y_exp, 'g-')
             # residual_analysis.residual_fft(y_data-y_exp)
-            ax.text(0.5, 0.95, "Period = %.2f" % params[4], horizontalalignment='center',verticalalignment='center', transform=ax.transAxes, fontsize=8)
-            rmse = accuracy.rmse(y_data, y_exp)
-            rsquare = accuracy.rsquare(y_data, y_exp)
-            rsquare_damp = accuracy.rsquare_damp(y_data, y_exp, x, params[2])
-            try:
-                peakDiffSquare = accuracy.peakDiffSquare(x, y_data, y_exp, params[3], params[4])
-                ax.text(0.5, 0.85, "RMSE = %.4f R^2 = %.4f R^2d = %.4f\n PeakSquareDiff = %.4f" % (rmse, rsquare, rsquare_damp, peakDiffSquare) , horizontalalignment='center',verticalalignment='center', transform=ax.transAxes, fontsize=6)
-            except:
-                print("Unable to compute peakDiffSquare for experiment index "+str(idx))
-                ax.text(0.5, 0.85, "RMSE = %.4f R^2 = %.4f R^2d = %.4f" % (rmse, rsquare, rsquare_damp) , horizontalalignment='center',verticalalignment='center', transform=ax.transAxes, fontsize=6)
-            output = accuracy.criteria(params[4], rsquare, rsquare_damp)
-            ax.text(0.8, 0.8, output, horizontalalignment='center',verticalalignment='center', transform=ax.transAxes, fontsize=12)
+            # ax.text(0.5, 0.95, "Period = %.2f" % params[4], horizontalalignment='center',verticalalignment='center', transform=ax.transAxes, fontsize=8)
+            # rmse = accuracy.rmse(y_data, y_exp)
+            # rsquare = accuracy.rsquare(y_data, y_exp)
+            # rsquare_damp = accuracy.rsquare_damp(y_data, y_exp, x, params[2])
+            # try:
+            #     peakDiffSquare = accuracy.peakDiffSquare(x, y_data, y_exp, params[3], params[4])
+            #     ax.text(0.5, 0.85, "RMSE = %.4f R^2 = %.4f R^2d = %.4f\n PeakSquareDiff = %.4f" % (rmse, rsquare, rsquare_damp, peakDiffSquare) , horizontalalignment='center',verticalalignment='center', transform=ax.transAxes, fontsize=6)
+            # except:
+            #     print("Unable to compute peakDiffSquare for experiment index "+str(idx))
+            #     ax.text(0.5, 0.85, "RMSE = %.4f R^2 = %.4f R^2d = %.4f" % (rmse, rsquare, rsquare_damp) , horizontalalignment='center',verticalalignment='center', transform=ax.transAxes, fontsize=6)
+            # output = accuracy.criteria(params[4], rsquare, rsquare_damp)
+            # ax.text(0.8, 0.8, output, horizontalalignment='center',verticalalignment='center', transform=ax.transAxes, fontsize=12)
         plt.show()
     # otherwise plot all experiments in the list
     else:
